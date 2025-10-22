a63324e30113093e8aa4204547ee7953d71df54c
Olger Cascante <olgerca100@gmail.com>
2025-10-21 18:27:58 -0600

    Respuesta

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
- 
+  get 'reviews/new'
+  get 'reviews/create'
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -12,6 +14,7 @@ Rails.application.routes.draw do
 
   resources :lists, only: [:index, :show, :new, :create] do
     resources :bookmarks, only: [:new, :create]
+    resources :reviews, only: [:new, :create]
   end
 
   resources :bookmarks, only: [:destroy]
